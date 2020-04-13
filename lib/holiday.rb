487d575e5b15352bbf92637ca4f770e21b787ba6
Sam Tamburri <tamburrisam@gmail.com>
2020-04-13 15:54:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,11 +53,13 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:"
+    puts season.to_s.capitalize! << ":"
     holiday.each do |holiday, supplies|
-    puts "#{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+      supplies = supplies.join(", ")
+      holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ")<< ":"
+      puts "  " + holiday + " " + supplies
+    end
   end
-end
 end
 
 def all_holidays_with_bbq(holiday_hash)
